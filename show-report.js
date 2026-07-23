5c75fbc762b67a226c6ccb45e57dce30c5250d95
Suweb-SCT <asuweb@schertech.com>
2026-07-23 11:33:45 +0600

    Enhance PDF report opening with Chrome support

--- show-report.js
+++ show-report.js
@@ -4,6 +4,16 @@ const path = require('path');
 const fs = require('fs');
 const { spawn } = require('child_process');
 
+const CHROME_PATHS_WIN = [
+  'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
+  'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+  path.join(process.env.LOCALAPPDATA || '', 'Google\\Chrome\\Application\\chrome.exe'),
+];
+
+function findChromeWin() {
+  return CHROME_PATHS_WIN.find((p) => p && fs.existsSync(p));
+}
+
 function openFile(filePath) {
   const platform = process.platform;
   const absPath = path.resolve(filePath);
@@ -11,13 +21,19 @@ function openFile(filePath) {
   let cmd;
   let args;
   if (platform === 'win32') {
-    cmd = 'explorer';
-    args = [absPath];
+    const chromePath = findChromeWin();
+    if (chromePath) {
+      cmd = chromePath;
+      args = [absPath];
+    } else {
+      cmd = 'explorer';
+      args = [absPath];
+    }
   } else if (platform === 'darwin') {
     cmd = 'open';
-    args = [absPath];
+    args = ['-a', 'Google Chrome', absPath];
   } else {
-    cmd = 'xdg-open';
+    cmd = 'google-chrome';
     args = [absPath];
   }
 
